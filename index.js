c977eb84a362ea8cb4d14cf7444b11738f89295c
Sumit Kumar <sumitkmr93@outlook.com>
2019-02-15 12:12:42 +0000

    added docker file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express'),
   app = express(),
   bodyParser = require('body-parser');
-  port = process.env.PORT || 3500;
+  port = process.env.PORT || 2500;
 
 
 const mysql = require('mysql');
