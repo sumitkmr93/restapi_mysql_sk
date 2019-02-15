be7925157a449690c532d20de524bc8e4fc2e4ba
Sumit Kumar <sumitkmr93@outlook.com>
2019-02-15 12:28:09 +0000

    port changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express'),
   app = express(),
   bodyParser = require('body-parser');
-  port = process.env.PORT || 2500;
+  port = process.env.PORT || 8081;
 
 
 const mysql = require('mysql');
