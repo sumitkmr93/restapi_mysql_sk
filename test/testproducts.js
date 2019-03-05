49bba93b91e6b085bd724ff06f065f5161d836b1
Sumit Kumar <Sumit.Kumar@version1.com>
2019-03-05 15:00:25 +0000

    changed

---
+++ test/testproducts.js
@@ -0,0 +1,66 @@
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+let should = chai.should();
+//The first test attempts to get all the bookings.
+describe('/GET all products', () => {
+  it('it should GET all the bookings', (done) => {
+    chai.request('https://productsdemoazure.azurewebsites.net/products')
+        .get('/')
+        .end((err, res) => {
+              res.should.have.status(200);
+              res.body.should.be.a('array');
+              console.log('First test passed');
+          done();
+        });
+  });
+});
+
+describe('/POST add a product', () => {
+  it('Post a booking', (done) => {
+      let product = {
+          name : "TestProduct",
+          price : 20,
+          stock : 100
+      }
+      chai.request('https://productsdemoazure.azurewebsites.net/products')
+        .post('/')
+        .send(product)
+        .end((err, res) => {
+              res.should.have.status(200);
+              res.body.should.be.a('object');
+          done();
+        });
+  });
+});
+
+// error when not updating numberofbags
+describe('/PUT change a product', () => {
+  it('PUT a booking', (done) => {
+      let product = {
+        name: "TestProduct",
+      }
+      chai.request('https://productsdemoazure.azurewebsites.net/products')
+        .put('/1')
+        .send(product)
+        .end((err, res) => {
+              res.should.have.status(200);
+              res.body.should.be.a('object');
+          done();
+        });
+  });
+});
+
+describe('/DELETE a product', () => {
+  it('DELETE a booking', (done) => {
+      chai.request('https://productsdemoazure.azurewebsites.net/products')
+        .delete('/1')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          res.body.should.have.property('message');
+          done();
+        });
+  });
+});
+
